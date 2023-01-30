a58eb9169007e148d4819f888ce962c7629e71f4
Torsten Simon <anfragen@tsgames.de>
2023-01-30 21:39:20 +0100

    init

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ app = FastAPI()
 async def query(query: str):
     openai.api_key = os.getenv("OPENAI_API_KEY")
     result = openai.Completion.create(
-      model="text-davinci-001",
-      prompt="Q: "+query,
-      temperature=0,
-      max_tokens=64,
+      model="text-davinci-003",
+      prompt="Q: "+query+"\nA:",
+      temperature=0.5,
+      max_tokens=128,
       top_p=1,
       frequency_penalty=0,
       presence_penalty=0,
